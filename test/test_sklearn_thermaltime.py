7307e0f1c1b50362d234534984f1ed4d286b1b85
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-08-10 17:21:01 +0200

    draft pycaret test

--- test/test_sklearn_thermaltime.py
+++ test/test_sklearn_thermaltime.py
@@ -5,6 +5,9 @@ https://scikit-learn.org/stable/developers/develop.html#rolling-your-own-estimat
 import unittest
 from sklearn.utils.estimator_checks import check_estimator
 from pyPhenology.models.sklearn_thermaltime import SklearnThermalTime
+from pycaret.regression import RegressionExperiment, save_model, load_model, predict_model
+from pyPhenology import utils
+import pandas as pd
 
 
 class TestSklearnCompliance(unittest.TestCase):
@@ -32,9 +35,29 @@ class TestSklearnCompliance(unittest.TestCase):
                         # twice.. but diffs are small
                         self.skipTest(f"Skipping {name}.")
 
-    # TODO add the test with real sample dataset
-    # def test_pycaret_compatible(self):
 
+class TestPyCaretCompliance():
+    # TODO add the test with real sample dataset
+    def test_pycaret_compatible(self, tmp_path):
+        # Use pyphenology test data
+        observations, temp = utils.load_test_data(name='vaccinium')
+        df = pd.DataFrame(
+            data={
+                "doy": observations["doy"],
+                "temp": temp["temperature"]}
+                )
+        df.dropna(inplace=True)
+
+        exp = RegressionExperiment()
+        exp.setup(df, target='doy', session_id=123)
+        model = exp.create_model(SklearnThermalTime(), cross_validation=False)
+        # # - ValueError: _CURRENT_EXPERIMENT global variable is not set. Please
+        # #   run setup() first.
+        save_model(model, tmp_path / "pycaret_thermaltime")
+
+        # loaded_model = load_model(tmp_path / "pycaret_thermaltime")
+        # loaded_model.setup(df, target='doy', session_id=123)
+        # loaded_model.predict_model(data =temp["temperature"])
 
 if __name__ == "__main__":
     unittest.main()
